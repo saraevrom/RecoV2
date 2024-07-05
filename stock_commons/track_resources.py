a9df8347d4ce309a865882c6f1db17a27920a225
Roman Saraev <saraevrom@gmail.com>
2024-07-05 19:25:03 +0300

    implemented missing method for manual X0Y0 resource

--- stock_commons/track_resources.py
+++ stock_commons/track_resources.py
@@ -69,6 +69,7 @@ class PositionPrior(object):
 
 class AutoPositionPrior(PositionPrior, BlankResource):
     Label = "Chooses automatically according to chosen pixels"
+
     def get_detector_prior(self,x_name,y_name,detector):
         minx,maxx,miny,maxy = detector.get_active_bounds()
         x_prior = pm.Uniform(x_name, minx, maxx)
@@ -83,6 +84,11 @@ class ManualPositionPrior(PositionPrior,CombineResource):
         "y_prior": dict(display_name="Y", type_=DistributionResource)
     })
 
+    def get_detector_prior(self,x_name,y_name,detector):
+        # minx,maxx,miny,maxy = detector.get_active_bounds()
+        x_prior = self.data.get_resource("x_prior").create_distribution(x_name)
+        y_prior = self.data.get_resource("y_prior").create_distribution(y_name)
+        return x_prior, y_prior
 
 class PositionPriorAlternate(AlternatingResource):
     Variants = [
