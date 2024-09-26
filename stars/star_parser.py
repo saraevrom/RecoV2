c2053cb7bf41c297deebf68d47b51f203709b598
Roman Saraev <saraevrom@gmail.com>
2024-09-26 18:24:58 +0300

    Extracted parsing single star function

--- stars/star_parser.py
+++ stars/star_parser.py
@@ -4,7 +4,7 @@ from typing import Type
 from .star import Star, StarList
 
 BAYER_LIST = "Alp Bet Gam Del Eps Zet Eta The Iot Kap Lam Mu Nu Xi Omi Pi Rho Sig Tau Ups Phi Chi Psi Ome".split(" ")
-BAYER_LETTER_PART = "(?:"+"|".join(BAYER_LIST)+")"
+BAYER_LETTER_PART = "(?:" + "|".join(BAYER_LIST) + ")"
 BAYER_REGEX = rf"{BAYER_LETTER_PART}(?:-?\d)?"
 
 
@@ -12,6 +12,7 @@ def wrap_star(star):
     if star is None:
         return None
     return [star]
+
 
 def match_by_group(match, field):
     group = match.group()
@@ -23,7 +24,7 @@ def match_by_group(match, field):
 def match_by_first_group(match, field, cast_type: Type = int):
     target = cast_type(match.groups()[0])
     #print("MATCH", field, target)
-    star = Star.fetch_sql(f"SELECT * FROM stars WHERE {field}==?",[target])
+    star = Star.fetch_sql(f"SELECT * FROM stars WHERE {field}==?", [target])
     return wrap_star(star)
     #res = database[database[field] == target]
     #print(res)
@@ -36,7 +37,7 @@ def match_by_dual(match, field1, field2, cast1: Type = str, cast2: Type = str, g
     grps = match.groups()
     target1 = cast1(grps[grp1])
     target2 = cast2(grps[grp2])
-    star = Star.fetch_sql(f"SELECT * FROM stars WHERE {field1}=? AND {field2}=?", [target1,target2])
+    star = Star.fetch_sql(f"SELECT * FROM stars WHERE {field1}=? AND {field2}=?", [target1, target2])
     return wrap_star(star)
     #print(f"targets {target1}; {target2}")
     # m1 = database[field1] == target1
@@ -47,10 +48,10 @@ def match_by_dual(match, field1, field2, cast1: Type = str, cast2: Type = str, g
 
 def match_sql_where(match):
     grp = match.groups()[0]
-    print("SQL GRP",grp)
+    #print("SQL GRP", grp)
     if grp:
         try:
-            stars = StarList.fetch_sql("SELECT * FROM stars WHERE "+grp,[]).stars
+            stars = StarList.fetch_sql("SELECT * FROM stars WHERE " + grp, []).stars
             if stars:
                 return stars
             return None
@@ -59,16 +60,17 @@ def match_sql_where(match):
     else:
         return None
 
+
 filters = [
     [r"\s+", None],
-    [r"WHERE\s*\(([^)]*)\)",match_sql_where],
+    [r"WHERE\s*\(([^)]*)\)", match_sql_where],
     #[r"Gliese\s*(\d+)", lambda x: match_by_first_group(x, "gl")],
     #[r"HIP\s*(\d+)", lambda x: match_by_first_group(x, "hip")],
     [r"HR\s*(\d+)", lambda x: match_by_first_group(x, "hr")],
     [r"HD\s*(\d+)", lambda x: match_by_first_group(x, "hd")],
     #[r"Gl\s*(\d+)", lambda x: match_by_first_group(x, "gl")],
-    [rf'({BAYER_REGEX})\s+(\w+)', lambda x: match_by_dual(x,"bayer","constellation")], # Bayer match
-    [r'(\d+)\s+(\w+)', lambda x: match_by_dual(x,"flamsteed","constellation", cast1=int)], # Flamsteed match
+    [rf'({BAYER_REGEX})\s+(\w+)', lambda x: match_by_dual(x, "bayer", "constellation")],  # Bayer match
+    [r'(\d+)\s+(\w+)', lambda x: match_by_dual(x, "flamsteed", "constellation", cast1=int)],  # Flamsteed match
     [r"\w+", lambda x: match_by_group(x, "proper")],
 ]
 
@@ -85,14 +87,7 @@ def parse_stars(asked_string):
     stars = StarList.new_empty()
     #database = get_database()
     while ptr < len(asked_string):
-        mat_pair = None
-        for filt in filters:
-            regex: re.Pattern
-            regex, processor = filt
-            mat = regex.match(asked_string[ptr:])
-            if mat:
-                mat_pair = mat, processor
-                break
+        mat_pair = match_one_star(asked_string, ptr)
         if mat_pair is None:
             #print("Unknown pattern on", ptr)
             return None
@@ -110,3 +105,29 @@ def parse_stars(asked_string):
                     return None
     #print("PARSE COMPLETED")
     return stars
+
+
+def match_one_star(asked_string, ptr=0):
+    mat_pair = None
+    for filt in filters:
+        regex: re.Pattern
+        regex, processor = filt
+        mat = regex.match(asked_string[ptr:])
+        if mat:
+            mat_pair = mat, processor
+            break
+    return mat_pair
+
+def parse_one_star(asked_string):
+    mat_pair = match_one_star(asked_string)
+    if mat_pair is None:
+        return None
+    mat, proc = mat_pair
+    if proc is None:
+        return None
+    add_stars = proc(mat)
+    if add_stars is None:
+        return None
+    if len(add_stars) == 0:
+        return None
+    return add_stars[0]
