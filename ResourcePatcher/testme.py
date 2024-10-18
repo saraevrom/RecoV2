2de2fa7b770ab29dcd0a5bc44a97f2a36f45a30a
Roman Saraev <saraevrom@gmail.com>
2024-10-18 16:32:48 +0300

    Clarified stripping examples

--- ResourcePatcher/testme.py
+++ ResourcePatcher/testme.py
@@ -1,16 +1,20 @@
 import io
+
+# Stripping block of imports.
 # STRIP IMPORT
 import this
 from datetime import datetime
 
 # Test data
 
+# Strip and replace.
 # STRIP
 a = "Strip This"
 # REPLACE
 # print("Stripped!")
 # END
 
+# Strip and replace. Since this block is broken it won't work.
 # STRIP
 a1 = "Strip 1"
 # REPLACE
@@ -18,19 +22,25 @@ a1 = "Strip 1"
 a1 = "OOPS"
 # END
 
+# Bracelike strip.
 # STRIP
 x = "Strip 2"
 # END
 
+
+# One line stripping
 # STRIP
 y = "Strip 3"
 
+# Another line stripping.
 # STRIP
 z = "Strip 4"
 
+# Stripping entire class.
 # STRIP CLASS
 class RemoveClass(object):
     def __init__(self, azaza):
         self.azaza = azaza
 
-x = 0
+# Stripping won't touch this line
+x42 = 0
